bb9423c5c0fa7f1d57ae8b912d44da66c1c9d16c
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-03-04 07:37:52 -0600

    abstraction_and_data_usage

---
+++ abstraction_and_data_usage/basic_apply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Ingresa tu nombre \n";
+    string nombre;
+    cin>>nombre;
+    cout<<"Bienvenido"<<nombre<<"al curso."<<endl;
+
+    return 0;
+}
