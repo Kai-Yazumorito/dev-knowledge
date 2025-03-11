2238ed4c1eb0a4726f6df4a56e35a866a1faee66
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-03-11 13:32:31 -0600

    .

---
+++ abstraction_and_data_usage/array-calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+// Main function
+int main() {
+    // array declaration
+    int gryffindor[n]={};
+    
+    // Variables
+    int i=0;   // Counter variable
+    int n=0;   // Array size
+    int option=0;   // User option
+    int sum=0;   // Sum of the array elements
+    int minus=0;   // Substraction of the array elements
+    int mult=1;   // Multiplication of the array elements
+    int div=1;   // Division of the array elements
+    int mod=0;   // Modulus of the array elements
+
+    // Instruccion for the user. u can change it if u want.
+    
+}
