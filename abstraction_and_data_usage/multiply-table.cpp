84a4217c095392eb27fc7be9d1790449568da8dc
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-03-14 07:47:51 -0600

    .

--- abstraction_and_data_usage/multiply-table.cpp
+++ abstraction_and_data_usage/multiply-table.cpp
@@ -3,17 +3,30 @@
 using namespace std;
 
 
+// Variable Declaration
+int multiply;   // Variable to save the result of the operation.
+int value;  // Variable to save the number the user type.
+
+
+// Array Declaration
+int hufflepuff[10][2];
+int ravenclaw[10];
+
+
 // Main function
 int main() {
     cout<<"The program made a multiply table."<<endl;
     cout<<"Type the number you want to multiply: ";
-    int value;
     cin>>value;
     cout<<"\n";
 
+    for(int i=0; i<10; i++) {
+        multiply=value*(i+1);   // Multiply operation.
+        ravenclaw[i]=multiply;
+//        cout<<ravenclaw[i]<<" , ";      // Function verifier.
+    }
+
     for(int i=1; i<=10; i++) {
-        int multiply;
-        multiply=value*i;
-        cout<<i<<" x "<<value<<" = "<<multiply<<"\n";
+        cout<<i<<" x "<<value<<" = "<<ravenclaw[i]<<"\n";
     }
 }
