44a79e908b99b06660e656681342283c6e49b4f8
Kai-Yazumorito <agarciafranco03@gmail.com>
2025-05-22 13:41:34 -0600

    python

---
+++ bb-101/repaso_final.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include <locale.h>
+using namespace std;
+
+
+// Estructura básica de prueba para determinar multiples variables
+typedef struct {
+    char name;
+    char phone;
+} person;
+
+
+
+int main() {
+    system("pause");        //  To pause the program.
+    printf("Hola mundo");   //  To show a message on the screen.
+    }
