cafc2cd12bc6ed2f2a631e5ed31bffa2197a2edf
Anhur Gallowsong <113324253+Kai-Yazumorito@users.noreply.github.com>
2025-05-30 01:06:28 +0000

    .

---
+++ python101/01_hello.py
@@ -0,0 +1,34 @@
+print("Hello, world :D")
+
+name="Kai"
+age=50
+
+lista=[1,2,3,4,5,6,7,8,9]
+
+print(lista[4])
+
+lista2=lista
+
+lista.append(15)
+
+# append es para agregar un dato extra a un array pero al final
+
+print(lista2)
+
+
+lista.append(2)
+
+
+print(lista.count(2))
+
+
+lista.insert(0, num)
+# inser es para agregar datos a un array pero de manera manual por lo que se debe de dar la posición.
+print(lista)
+
+print(lista.pop())
+# pop saca el ultimo dato registrado en el array
+
+lista_combinada = [3, 'Aldo', 25.68, True]
+print(lista_combinada)
+
